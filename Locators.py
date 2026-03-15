b55b8d6cf7121a81bfe37049d49173b9e0072528
iamakash45 <135732227+iamakash45@users.noreply.github.com>
2026-03-15 15:55:44 +0530

    Locators.py

---
+++ Locators.py
@@ -0,0 +1,15 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://rahulshettyacademy.com/client")
+
+driver.find_element(By.LINK_TEXT, "Forgot password?").click()
+driver.find_element(By.XPATH, "//form/div[1]/input").send_keys("demo@gmail.com")
+driver.find_element(By.CSS_SELECTOR, "form div:nth-child(2) input").send_keys("Hello@1234")
+driver.find_element(By.CSS_SELECTOR, "#confirmPassword").send_keys("Hello@1234")
+driver.find_element(By.XPATH, "//button[@type='submit']").click()
+time.sleep(5)
