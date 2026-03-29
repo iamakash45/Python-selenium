26c1632404f136d7aca063bd4f093a624eee00b6
iamakash45 <135732227+iamakash45@users.noreply.github.com>
2026-03-29 10:12:25 +0530

    autosuggestive.py
    Implement autosuggestive country selection using Selenium.

---
+++ Autosuggestive.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/dropdownsPractise/")
+driver.maximize_window()
+
+driver.find_element(By.ID, "autosuggest").send_keys("ind")
+time.sleep(3)
+countries = driver.find_elements(By.CSS_SELECTOR, "li[class='ui-menu-item'] a")
+print(len(countries))
+
+for country in countries:
+    if country.text == "India":
+        country.click()
+        break
