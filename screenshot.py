33be1133feca7f2724f36d64a884b1a9877f2061
iamakash45 <135732227+iamakash45@users.noreply.github.com>
2026-06-29 21:27:33 +0530

    Add screenshot functionality using Selenium

---
+++ screenshot.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(2)
+
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.execute_script("window.scrollBy(0, document.body.scrollHeight);")
+driver.get_screenshot_as_file("screen.png")
